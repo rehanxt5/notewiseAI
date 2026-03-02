7bb5fa845d62a288da54ff7498fe903cb883675b
rehanxt5 <rehan.xt5@gmail.com>
2026-03-02 16:00:09 +0530

    added sparse retrieval for bge-m3 model

--- ai/retriever/simpleretriever.py
+++ ai/retriever/simpleretriever.py
@@ -3,6 +3,7 @@ docstring for ai.retriever.simpleretriever.SimpleRetriever
 '''
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
+from FlagEmbedding import BGEM3FlagModel
 class SimpleRetriever:
     '''
     A simple retriever that uses cosine similarity to retrieve relevant documents based on query embeddings.
@@ -26,4 +27,20 @@ class SimpleRetriever:
         # Get the indices of the top_k most similar documents
         top_k_indices = np.argsort(similarities)[-top_k:][::-1]
         return top_k_indices.tolist()
-    
+
+    @staticmethod
+    def sparse_retrieve(model,query_vector:np.ndarray, document_vectors:np.ndarray, top_k=5):
+        '''
+        Retrieve the top_k most relevant documents based on cosine similarity between query and document vectors.
+
+        Args:
+            query_vector (np.array): The sparse vector for the query. [1D array]
+            document_vectors (np.array): A 2D array of sparse vectors for the documents. [2D array]
+            top_k (int): The number of top relevant documents to retrieve.
+        Returns:
+            list: A list of indices corresponding to the top_k most relevant documents.
+        '''
+        similarities = model.cosine_similarity(query_vector, document_vectors)
+        top_k_indices = np.argsort(similarities)[-top_k:][::-1]
+        return top_k_indices.tolist()
+     
